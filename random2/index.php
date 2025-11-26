335c7ebe17227b36518e3e7362425b33ce0c26ae
crossi72 <claudio@eurosoftlab.com>
2025-11-26 16:35:56 +0100

    added index.php and style.css with dynamic div generation and styling

---
+++ random2/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>contenuti a caso!</h1>
+	<?php
+		// Genera un numero casuale tra 10 e 30
+		$numDivs = rand(10, 30);
+
+		// Crea i div in base al numero generato
+		for ($i = 1; $i <= $numDivs; $i++) {
+			if ($i % 2 == 0) {
+				// Div pari
+				printDiv("io sono pari", "pari");
+			} else {
+				// Div dispari
+				printDiv("io sono dispari", "dispari");
+			}
+		}
+
+		// Div finale
+		echo "<div class='ultimo'>fine pagina</div>";
+	?>
+</body>
+</html>
+
+<?php
+	/**
+	 * riceve una stringa e stampa un div con la stringa passata come contenuto
+	 * @param string $content Il contenuto da inserire nel div
+	 * @param string $class La classe da applicare al div
+	 * @return void
+	 */
+	function printDiv($content, $class) {
+		echo "<div class='$class'>$content</div>";
+	}
+?>
