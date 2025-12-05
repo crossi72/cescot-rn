b33d5ebc26778eea4dfd55d3f50d3268b014d3ab
crossi72 <claudio@eurosoftlab.com>
2025-12-05 11:51:05 +0100

    initial commit of index.php, script.js, and style.css with dynamic div generation and color change functionality

---
+++ events/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>eventi javascript</h1>
+	<button id="myButton">colora</button>
+	<?php
+	/*
+		stampo 20 div
+	*/
+		for ($i = 1; $i <= 20; $i++) {
+			printDiv("", "box");
+		}
+
+		function printDiv($content, $class) {
+			echo "<div class='$class'>$content</div>";
+		}
+	?>
+</body>
+<script src="script.js"></script>
+</html>
