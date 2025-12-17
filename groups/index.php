4855550c808435b8a53183a8d7f0c94fea410f72
crossi72 <claudio@eurosoftlab.com>
2025-12-17 12:39:32 +0100

    feat: add buttons for additional color groups and implement corresponding event listeners

--- groups/index.php
+++ groups/index.php
@@ -15,10 +15,16 @@
 		<?php
 			printButton("Primo Rosso", "btn_red", "btn_red_1");
 			printButton("Secondo Rosso", "btn_red", "btn_red_2");
+			printButton("Terzo Rosso", "btn_red", "btn_red_3");
 			printButton("Tutti Rossi", "btn_red", "btn_red_all");
 			printButton("Primo Blu", "btn_blue", "btn_blue_1");
 			printButton("Secondo Blu", "btn_blue", "btn_blue_2");
+			printButton("Terzo Blu", "btn_blue", "btn_blue_3");
 			printButton("Tutti Blu", "btn_blue", "btn_blue_all");
+			printButton("Primo Giallo", "btn_yellow", "btn_yellow_1");
+			printButton("Secondo Giallo", "btn_yellow", "btn_yellow_2");
+			printButton("Terzo Giallo", "btn_yellow", "btn_yellow_3");
+			printButton("Tutti Giallo", "btn_yellow", "btn_yellow_all");
 		?>
 		<h2>Primo gruppo</h2>
 		<?php
@@ -27,6 +33,10 @@
 		<h2>Secondo gruppo</h2>
 		<?php
 			printRandomDivs(10, 15, "", "div_100 group_2");
+		?>
+		<h2>Terzo gruppo</h2>
+		<?php
+			printRandomDivs(5, 20, "", "div_50 group_3");
 		?>
 	</div>
 </body>
