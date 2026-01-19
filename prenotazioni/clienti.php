7fa482f9f453d1e70705237f0a1b885ea018ef26
crossi72 <claudio@eurosoftlab.com>
2026-01-19 12:48:33 +0100

    feat: update clienti.php to use GET method for region selection and add pagination links

--- prenotazioni/clienti.php
+++ prenotazioni/clienti.php
@@ -20,7 +20,7 @@
 
 		//mostro una select che contiene tutte le regioni
 		?>
-		<form method=POST>
+		<form method=GET>
 			<label for='regione'>Seleziona una regione:</label>
 			<select name='regione' id='regione'>
 				<option value='0'>-- Seleziona una regione --</option>
@@ -34,18 +34,44 @@
 		</form>
 
 		<?php
-		//leggo l'ID della regione dal parametro POST
-		$regione_id = isset($_POST['regione']) ? intval($_POST['regione']) : 0;
+		//leggo l'ID della regione dal parametro GET
+		$regione_id = isset($_GET['regione']) ? intval($_GET['regione']) : 0;
+
+		$pagina = isset($_GET['pagina']) ? intval($_GET['pagina']) : 0;
+		if ($pagina < 0) {
+			$pagina = 0;
+		}
 
 		//eseguo una query per ottenere tutti i clienti con i dati della regione, area geografica e citta
+		$clienti_da_caricare = 50;
 		$query = "SELECT clienti.nome, clienti.cognome, regioni.regione,
 			regioni.area_geografica, citta.citta
 			FROM clienti
 			INNER JOIN citta ON clienti.citta = citta.id_citta
 			INNER JOIN regioni ON citta.regione = regioni.id_regione
-			WHERE regioni.id_regione = " . $regione_id;
+			WHERE regioni.id_regione = " . $regione_id . "
+			LIMIT " . $pagina * $clienti_da_caricare . ", " . $clienti_da_caricare;
 
 		$result = mysqli_query($db_connection, $query);
+
+		// aggiungo i link di navigazione
+		echo "<navigation>";
+			// se sono a pagina 0, non mostro il link "Indietro"
+			if ($pagina > 0){
+				echo "<a href='clienti.php?regione=" . $regione_id . 
+				"&pagina=". $pagina - 1 ."'>Indietro</a>";
+			} else {
+				echo "Indietro";
+			}
+			echo " | ";
+			//se sono nell'ultima pagina, non mostro il link "Avanti"
+			if (mysqli_num_rows($result) < ($pagina + 1) * 50){
+				echo "Avanti<br><br>";
+			} else{
+				echo "<a href='clienti.php?regione=" . $regione_id . 
+				"&pagina=". $pagina + 1 ."'>Avanti</a><br><br>";
+			}
+		echo "</navigation>";
 
 		//ciclo sulle righe restituite e stampo i dati di ogni cliente
 		while ($row = mysqli_fetch_assoc($result)) {
